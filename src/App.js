785d7cf1cd6adaa0857cb95a1b1cd5efeaedb1b5
Hernan Pereira <HP#simplistic#1105>
2022-03-10 23:32:07 -0300

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import CssBaseline from '@mui/material/CssBaseline'
+import Container from '@mui/material/Container'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Store</p>
-      </header>
-    </div>
-  );
+    <>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Container>
+          <p>Store Content</p>
+        </Container>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
